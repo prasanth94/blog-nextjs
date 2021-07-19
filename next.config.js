dd1b0b6f9485c48745460f6283af0ed039903ef7
Prasanth <prasanth@cloudthing.com>
2021-07-19 23:54:14 +0530

    removing env variables

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  if (PHASE_DEVELOPMENT_SERVER === phase) {
-    return {
-      env: {
-        mongodb_username: "cosmos",
-        mongodb_password: "Hustle@2021",
-        mongodb_cluster: "cluster0",
-        mongodb_database: "blog-contact",
-      },
-      reactStrictMode: true,
-    };
-  } else
-    return {
-      env: {
-        mongodb_username: "cosmos",
-        mongodb_password: "Hustle@2021",
-        mongodb_cluster: "cluster0",
-        mongodb_database: "blog-contact",
-      },
-      reactStrictMode: true,
-    };
+module.exports = {
+  reactStrictMode: true,
 };
