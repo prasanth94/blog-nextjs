1dc520a08b261b09eafe035c2a48a3317c701c78
Prasanth <prasanth@cloudthing.com>
2021-07-20 23:54:31 +0530

    remove Head component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import Hero from "../components/home/hero";
-import Head from "next/Head";
 import FeaturedPosts from "../components/home/featured-posts";
 import { getFeaturedPosts } from "../lib/post-util";
 export default function HomePage(props) {
   return (
     <>
-      <Head>
-        <title>Prasanth's blog</title>
-        <meta
-          name="description"
-          content="This is a blog on javascript and specifically about react"
-        />
-      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
